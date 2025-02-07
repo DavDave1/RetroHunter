72b40f43806249d6e6e26d39dec675b113f14fd6
Davide Davelli <davellidavide@gmail.com>
2025-02-07 16:45:39 +0100

    fix compile issue in tests

--- RaSetMaker.Tests/MatchersTests/Md5MatcherTests.cs
+++ RaSetMaker.Tests/MatchersTests/Md5MatcherTests.cs
@@ -21,20 +21,5 @@ namespace RaSetMaker.Tests.MatchersTests
             Assert.Equal(expectedHash, foundRom.Hash);
 
         }
-
-        [Theory]
-        [InlineData("../../../TestRoms/atari2600.zip", "0db4f4150fecf77e4ce72ca4d04c052f")]
-        public void RomExtensionDoesNotMatchSystem(string filePath, string expectedHash)
-        {
-            var sys = GetGameSystemByName("Atari 2600");
-            sys.SupportedExtensions = [".rom"]; // This should not match
-            AddRomWithHash(sys, expectedHash);
-
-            var matcher = RomMatcherFactory.Create(sys);
-
-            var (foundRom, _) = matcher.FindRom(new FileInfo(filePath));
-
-            Assert.Null(foundRom);
-        }
     }
 }
