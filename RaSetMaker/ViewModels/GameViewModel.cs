e12caca5b901f6cb5a4d0152116361207c018094
Davide Davelli <davellidavide@gmail.com>
2025-03-19 14:45:01 +0100

    Display games list as data grid

--- RaSetMaker/ViewModels/GameViewModel.cs
+++ RaSetMaker/ViewModels/GameViewModel.cs
@@ -1,46 +1,50 @@
 ﻿using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using RaSetMaker.Models;
 using RaSetMaker.Utils;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace RaSetMaker.ViewModels
 {
-    public partial class GameViewModel : TreeViewItemModel
+    public partial class GameViewModel : ViewModelBase
     {
         [ObservableProperty]
         private Bitmap? _gameIcon;
+
+        [ObservableProperty]
+        private Bitmap? _statusIcon;
+
+        [ObservableProperty]
+        private string _gameTypes;
+
+        [ObservableProperty]
+        private List<RomViewModel> _roms;
 
         public Game Game { get; private set; }
 
         public int ValidRomsCount { get; private set; }
 
-        public override string IconSrc => ValidRomsCount == 0 ? "avares://RaSetMaker/Assets/error.png" : "avares://RaSetMaker/Assets/check.png";
-
-        public List<RomViewModel> Roms => Children.Select(c => (RomViewModel)c).ToList();
-
-        public GameViewModel(MainViewModel mainVm, Game game, UserConfig userConfig) : base(mainVm)
+        public GameViewModel(MainViewModel mainVm, Game game, UserConfig userConfig) : base()
         {
             Game = game;
             _mainVm = mainVm;
             _userConfig = userConfig;
             var romViewModels = Game.Roms.Select(r => new RomViewModel(mainVm, r, userConfig)).OrderBy(r => r.Title).ToList();
             ValidRomsCount = romViewModels.Sum(rvm => rvm.IsRomValid ? 1 : 0);
-            Children = [.. romViewModels.Select(r => (TreeViewItemModel)r)];
-            Title = Game.GameTypes.Count == 0 ? Game.Name : $"{Game.Name} {GameTypesToString()}";
+            Roms = romViewModels;
+            GameTypes = GameTypesToString();
+            var iconSrc = ValidRomsCount == 0 ? "avares://RaSetMaker/Assets/error.png" : "avares://RaSetMaker/Assets/check.png";
+            StatusIcon = ImageHelper.LoadFromResource(new Uri(iconSrc));
         }
 
         public async Task LoadDetails()
         {
             GameIcon = await ImageHelper.LoadFromWeb(new Uri(Game.IconUrl));
             await _mainVm.UpdateGameData(Game);
-            Children = [.. Game.Roms.Select(r => new RomViewModel(_mainVm, r, _userConfig))];
+            Roms = [.. Game.Roms.Select(r => new RomViewModel(_mainVm, r, _userConfig))];
         }
 
         private string GameTypesToString() => string.Join("", Game.GameTypes.Select(t => $"[{t}]").Order());
