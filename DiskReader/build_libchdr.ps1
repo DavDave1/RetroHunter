40dff04133dfebf45537eb207084216482702302
Davide Davelli <davellidavide@gmail.com>
2025-12-21 14:11:46 +0100

    fix libchdr windows build
    load VS environment in build_libchdr.ps1 script

--- DiskReader/build_libchdr.ps1
+++ DiskReader/build_libchdr.ps1
@@ -1,5 +1,20 @@
 echo "Building libchdr dependency for current system"
 
+# Find the installation path of Visual Studio
+$vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+
+if (-not $vsPath) {
+    Write-Error "Visual Studio with C++ build tools not found."
+    exit 1
+}
+
+# Path to the Dev Shell module
+$devShellPath = Join-Path $vsPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+
+# Import and initialize the environment (targeting x64)
+Import-Module $devShellPath
+Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation -Arch amd64
+
 $srcDir = $PSScriptRoot + "\\libchdr"
 $buildDir = $srcDir + "\\build"
 
